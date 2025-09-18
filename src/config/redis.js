5fbd721bc4fa0a9ad36ad6779f81b5787154d306
404Ameyy <139114740+Ameysr@users.noreply.github.com>
2025-09-18 22:09:31 +0530

    db update

--- src/config/redis.js
+++ src/config/redis.js
@@ -5,10 +5,9 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-15681.crce206.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 15681
+        host: 'redis-10897.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 10897
     }
 });
-
 
 module.exports = redisClient;
